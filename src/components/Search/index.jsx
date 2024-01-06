e331851ad193305ce615803e5debc03604fb115b
Dmitrii Privezencev <dv.privezencev@mail.ru>
2024-01-06 17:49:22 +0300

    redux-toolkit, axios

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useRef } from 'react'
 import { SearchContext } from '../../App'
 
 import st from './Search.module.scss'
@@ -7,9 +7,18 @@ const Search = () => {
 
 	const { searchValue, setSearchValue } = useContext(SearchContext)
 
+	const inputRef = useRef()
+
+
+	const clearSearch = () => {
+		setSearchValue('')
+		inputRef.current.focus()
+	}
+
 	return (
 		<div className={st.root}>
 			<input
+				ref={inputRef}
 				className={st.input}
 				placeholder='Поиск пиццы'
 				onChange={(e) => setSearchValue(e.target.value)}
@@ -21,7 +30,7 @@ const Search = () => {
 				height="48"
 				viewBox="0 0 48 48"
 				width="48"
-				onClick={() => setSearchValue('')} >
+				onClick={clearSearch} >
 				<path d="M38 12.83L35.17 10 24 21.17 12.83 10 10 12.83 21.17 24 10 35.17 12.83 38 24 26.83 35.17 38 38 35.17 26.83 24z" />
 				<path d="M0 0h48v48H0z" fill="none" />
 			</svg>}
